47497429d041cf3c872685e0607b4de85267e437
Almond8976 <anand.bharani11@gmail.com>
2025-11-08 17:13:36 -0600

    additions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Sparky.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Sparky.java
@@ -6,20 +6,43 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Sparky {
 
+    public int ballCount = 0;
     NormalizedColorSensor colorSensor;
 
     public enum DetectedColors {
         GREEN,
-        PURPLE
+        BLUE,
+        PURPLE,
+        UNKNOWN
     }
 
-    public void init(HardwareMap HwMap) {
-        colorSensor = hardwareMap.get(NormalizedColorSensor.class "colorSensor");
+    public void init(HardwareMap hwMap) {
+        colorSensor = hwMap.get(NormalizedColorSensor.class, "colorSensor");
+        colorSensor.setGain(6);
     }
 
-    public DetectedColors getDetectedColor() {
+    public DetectedColors getDetectedColor(Telemetry telemetry) {
         NormalizedRGBA colors = colorSensor.getNormalizedColors();
+
+        float normGreen, normRed, normBlue;
+        normGreen = colors.green / colors.alpha;
+        normRed = colors.red / colors.alpha;
+        normBlue = colors.blue / colors.alpha;
+
+        telemetry.addData("green", normGreen);
+        telemetry.addData("red", normRed);
+        telemetry.addData("blue", normBlue);
+        telemetry.addData("Ball Count: ", ballCount);
+        telemetry.update();
+
+        if(normBlue > 0.1) {
+            ballCount++;
+        }
+
+        return DetectedColors.BLUE;
     }
 }
