07530570be2cfc5b0691b0e84a92e0b8f494b6e3
FormulaCarbon <99456163+FormulaCarbon@users.noreply.github.com>
2025-11-10 19:06:23 -0600

    velocity for shooter and testshooter opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Mortar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Mortar.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
@@ -12,14 +13,17 @@ import java.util.HashMap;
 public class Mortar {
     private DcMotorEx flyMotor;
 
-    private double power;
+    private double power, vel;
 
     public static double THRESH = 100;
     public static double OFF = 0, MAX = 1, NORMAL = 0.6;
 
+    public static double MAX_VEL = 1200;
+
     public Mortar(HardwareMap hardwareMap, HashMap<String, String> config) {
         flyMotor = hardwareMap.get(DcMotorEx.class, config.get("shooter"));
         flyMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        flyMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300, 0, 0, 10));
         flyMotor.setDirection(Direction.REVERSE);
     }
     public void setPower(double motorPower) {
@@ -27,11 +31,15 @@ public class Mortar {
     }
 
     public void setVelocity(double velocity) {
-        flyMotor.setVelocity(velocity);
+        vel = velocity;
     }
 
     public double getVelocity() {
-        return flyMotor.getVelocity(AngleUnit.DEGREES);
+        return flyMotor.getVelocity();
+    }
+
+    public double getTargetVelocity() {
+        return vel;
     }
 
     public double getPower() {
@@ -39,6 +47,7 @@ public class Mortar {
     }
 
     public void update() {
-        flyMotor.setPower(power);
+        //flyMotor.setPower(power);
+        flyMotor.setVelocity(vel);
     }
  }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestShooter.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.Kicker;
+import org.firstinspires.ftc.teamcode.subsystems.Mortar;
+import org.firstinspires.ftc.teamcode.subsystems.Util;
+
+@Config
+@TeleOp(name = "TestShooter")
+public class TestShooter extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        Util util = new Util();
+        Mortar shooter = new Mortar(hardwareMap, util.deviceConf);
+        Kicker kicker = new Kicker(hardwareMap, util.deviceConf);
+
+        double vel = 0;
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            if (gamepad1.a) {
+                vel = 0;
+            }
+            if (gamepad1.right_bumper) {
+                vel += 100;
+            }
+            if (gamepad1.left_bumper) {
+                vel -= 100;
+            }
+
+            if (gamepad2.x) {
+                kicker.setPosition(Kicker.DOWN);
+            }
+            if (gamepad2.y) {
+                kicker.setPosition(Kicker.UP);
+            }
+            
+            shooter.setVelocity(vel);
+            shooter.update();
+            kicker.update();
+
+            telemetry.addData("vel", shooter.getVelocity());
+            telemetry.addData("target vel", shooter.getTargetVelocity());
+            telemetry.update();
+
+        }
+    }
+}
