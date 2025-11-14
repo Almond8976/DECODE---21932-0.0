053b3e7391e821be105a880ef8e09180b260a9d5
ChocolateLord <kasuganti.abhay@gmail.com>
2025-11-13 21:33:18 -0600

    turret wrapper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr_wrappers/TurretWrapper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr_wrappers/TurretWrapper.java
@@ -1,5 +1,25 @@
 package org.firstinspires.ftc.teamcode.rr_wrappers;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+
 public class TurretWrapper {
-    public
+    public class Launch implements Action {
+        private boolean initialized = false;
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            if (!initialized) {
+                initialized = true;
+            }
+
+            // function to call when action is ran
+            shooter.setPower(power);
+            double pow = shooter.getPower();
+            packet.put("shooter power", pow);
+            return true;
+        }
+    }
 }
