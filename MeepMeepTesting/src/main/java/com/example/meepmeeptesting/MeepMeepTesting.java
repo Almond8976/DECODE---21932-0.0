3c42163b4416ee64d056a61f0414bee9ac790ba7
ChocolateLord <kasuganti.abhay@gmail.com>
2025-11-25 19:09:38 -0600

    15ball

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -12,12 +12,28 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(180, 180, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-57.78, 45.6439, Math.toRadians(128.188)))
                 .strafeToSplineHeading(new Vector2d(-12, 12), Math.toRadians(90))
                 //.strafeToConstantHeading(new Vector2d(-11, 33))
+                .strafeToConstantHeading(new Vector2d(-11, 53))
+                .strafeToConstantHeading(new Vector2d(-20, 20))
+                .strafeToConstantHeading(new Vector2d(13, 29))
+                .strafeToConstantHeading(new Vector2d(13, 60))
+                .strafeToSplineHeading(new Vector2d(-2, 54), Math.toRadians(0))
+                .strafeToConstantHeading(new Vector2d(-2, 60))
+                .strafeToSplineHeading(new Vector2d(-20, 20), Math.toRadians(90))
+                .strafeToConstantHeading(new Vector2d(36, 29))
+                .strafeToConstantHeading(new Vector2d(36, 60))
+                .strafeToSplineHeading(new Vector2d(-20, 20), Math.toRadians(0))
+                .strafeToConstantHeading(new Vector2d(24, 65))
+                .strafeToConstantHeading(new Vector2d(60, 65))
+                .strafeToConstantHeading(new Vector2d(-20, 20))
+                .strafeToConstantHeading(new Vector2d(0, 52))
+
+                /*
                 .strafeToConstantHeading(new Vector2d(-11, 53))
                 .turnTo(0)
                 .strafeToConstantHeading(new Vector2d(0, 54))
@@ -28,7 +44,7 @@ public class MeepMeepTesting {
                 .strafeTo(new Vector2d(-20, 20))
                 .strafeToConstantHeading(new Vector2d(36, 29))
                 .strafeToConstantHeading(new Vector2d(36, 60))
-                .strafeToConstantHeading(new Vector2d(-20, 20))
+                .strafeToConstantHeading(new Vector2d(-20, 20))*/
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto15BallRed.java
@@ -0,0 +1,209 @@
+
+package org.firstinspires.ftc.teamcode.auto;
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.rr_wrappers.IntakeWrapper;
+import org.firstinspires.ftc.teamcode.subsystems.Gate;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.Kicker;
+import org.firstinspires.ftc.teamcode.subsystems.Mortar;
+import org.firstinspires.ftc.teamcode.subsystems.Turret;
+import org.firstinspires.ftc.teamcode.subsystems.Util;
+
+@Config
+@Autonomous(name = "Auto15BallRed")
+public class Auto15BallRed extends LinearOpMode{
+
+    Util util;
+    Kicker kicker;
+    Mortar shooter;
+    Turret turret;
+    IntakeWrapper intakeWr;
+    Intake intake;
+    Gate gate;
+
+    private MecanumDrive drive;
+
+    public static int KICKER_WAIT_TIME = 500;
+
+    private int shooterTargetSpeed;
+    private double target;
+
+    ElapsedTime time1 = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // init all subsystems (switch to using wrappers if you want parallel movement)
+        util = new Util();
+        kicker = new Kicker(hardwareMap, util.deviceConf);
+        shooter = new Mortar(hardwareMap, util.deviceConf);
+        turret = new Turret(hardwareMap, util.deviceConf, new Pose2d(-57.78, 45.6439, Math.toRadians(128.188)));
+        intakeWr = new IntakeWrapper(hardwareMap, util.deviceConf);
+        intake = new Intake(hardwareMap, util.deviceConf);
+        gate = new Gate(hardwareMap, util.deviceConf);
+
+
+
+        // define startpose, in, in, rad
+        Pose2d startPose = new Pose2d(-57.78, 45.6439, Math.toRadians(128.188));
+        drive = new MecanumDrive(hardwareMap, startPose);
+        turret.setBasketPos(Turret.redBasket);
+        kicker.setPosition(Kicker.DOWN);
+
+        TrajectoryActionBuilder trajPreload = drive.actionBuilder(startPose)
+                .strafeToSplineHeading(new Vector2d(-12, 12), Math.toRadians(90));
+
+        TrajectoryActionBuilder trajSetOne = drive.actionBuilder(new Pose2d(new Vector2d(-12, 12), Math.toRadians(90)))
+                .afterTime(0, intakeWr.startIntake())
+                .strafeToConstantHeading(new Vector2d(-11, 53))
+                .afterTime(1, intakeWr.stopIntake())
+                .strafeToConstantHeading(new Vector2d(-20, 20));
+
+        TrajectoryActionBuilder trajSetTwo = drive.actionBuilder(new Pose2d(new Vector2d(-20, 20), Math.toRadians(90)))
+                .strafeToConstantHeading(new Vector2d(13, 29))
+                .afterTime(0, intakeWr.startIntake())
+                .strafeToConstantHeading(new Vector2d(13, 60))
+                .afterTime(1, intakeWr.stopIntake())
+                .strafeToSplineHeading(new Vector2d(-2, 54), Math.toRadians(0))
+                .strafeToConstantHeading(new Vector2d(-2, 60));
+
+        TrajectoryActionBuilder trajShootTwo = drive.actionBuilder(new Pose2d(new Vector2d(-2, 60), Math.toRadians(0)))
+                .strafeToSplineHeading(new Vector2d(-20, 20), Math.toRadians(90));
+
+        TrajectoryActionBuilder trajSetThree = drive.actionBuilder(new Pose2d(new Vector2d(-20, 20), Math.toRadians(90)))
+                .strafeToConstantHeading(new Vector2d(36, 29))
+                .afterTime(0, intakeWr.startIntake())
+                .strafeToConstantHeading(new Vector2d(36, 60))
+                .afterTime(1, intakeWr.stopIntake())
+                .strafeToSplineHeading(new Vector2d(-20, 20), Math.toRadians(0));
+
+        TrajectoryActionBuilder trajSetFour = drive.actionBuilder(new Pose2d(new Vector2d(-20, 20), Math.toRadians(0)))
+                .strafeToConstantHeading(new Vector2d(24, 65))
+                .strafeToConstantHeading(new Vector2d(60, 65))
+                .strafeToConstantHeading(new Vector2d(-20, 20));
+
+        TrajectoryActionBuilder trajLeave = drive.actionBuilder(new Pose2d(new Vector2d(-20, 20), Math.toRadians(0)))
+                .strafeToConstantHeading(new Vector2d(0, 52));
+
+        Thread update = new Thread( ()-> updateAll(turret, shooter, kicker, intake, gate, intakeWr));
+
+        // TODO: move everything to start position (after init, before program start)
+
+
+
+        waitForStart();
+        update.start();
+        shooter.setVelocity(shooter.calcVelocity(73.5391));
+        Turret.tracking = true;
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajPreload.build()
+                )
+        );
+
+        Launch();
+
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajSetOne.build()
+                )
+        );
+        Launch();
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajSetTwo.build()
+                )
+        );
+        sleep(600);
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajShootTwo.build()
+                )
+        );
+        Launch();
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajSetThree.build()
+                )
+        );
+        Launch();
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajSetFour.build()
+                )
+        );
+
+        Turret.tracking = false;
+        turret.setPosition(0);
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajLeave.build()
+                )
+        );
+
+    }
+    // Define all functions here (if you call subsystems movements from here it wont be parallel)
+
+    public void Launch() {
+
+
+
+        shooterTargetSpeed = shooter.calcVelocity(
+                Math.sqrt(
+                        (turret.distanceToBasket().x * turret.distanceToBasket().x) + (turret.distanceToBasket().y * turret.distanceToBasket().y)
+                )
+        );
+        //target = turret.getTargetPosition();
+        shooter.setVelocity(shooterTargetSpeed);
+
+        shooter.setVelocity(shooterTargetSpeed);
+        do {
+            gate.setPosition(Gate.OPEN);
+        }
+        while (shooter.getVelocity() < shooterTargetSpeed - Mortar.THRESH);
+        intake.setIntakePower(1);
+        sleep(KICKER_WAIT_TIME);
+        kicker.setPosition(Kicker.UP);
+        sleep(500);
+        kicker.setPosition(Kicker.DOWN);
+        gate.setPosition(Gate.CLOSE);
+
+        //Turret.tracking = false;
+        //turret.setPosition(0);
+
+
+    }
+
+    public void updateAll(Turret turret, Mortar shooter, Kicker kicker, Intake intake, Gate gate, IntakeWrapper intakeWr){
+        while (opModeInInit() || opModeIsActive())
+        {
+            shooter.update();
+            kicker.update();
+            turret.update();
+            intake.update();
+            gate.update();
+            intakeWr.update();
+
+            telemetry.addData("Heading", turret.getPose().heading.toDouble());
+            telemetry.addData("Gate Position", gate.getPosition());
+            telemetry.update();
+        }
+    }
+}
